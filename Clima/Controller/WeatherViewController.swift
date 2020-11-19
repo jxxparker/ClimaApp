fc4ce394970ae700e284f23072d493e7f6fe3231
Jihun Park <jxxparker@gmail.com>
2020-11-19 14:13:32 -0600

    half

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self //handles it so it works on keyboard and toggle board
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) { //for searchbar
+        searchTextField.endEditing(true) //keyboard disappears
+        print(searchTextField.text!)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool { //for keyboard
+        searchTextField.endEditing(true) //keyboard disappears
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool { //what happens if user tries to de-select editing
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) { //triggers after search has been edited
+        searchTextField.text = "" //clears the searchtextfield so you can search something new after
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+![Screen Shot 2020-11-19 at 2 11 40 PM](https://user-images.githubusercontent.com/29707177/99718866-2aa41700-2a71-11eb-882c-e779cf70d055.png)
+
+This is an application which tells you what degree is each city you search for.
+
+This application is created by using basic programming logic, delegate methods and API. 
